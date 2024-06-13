7e1259ff5bf36807bc9bf813b5b45a388597fdce
projectbidanmandiri <projectbidanmandiri@gmail.com>
2024-06-13 17:38:10 +0000

    add function reminder

---
+++ api/packages/api/reminder/__main__.py
@@ -0,0 +1,91 @@
+from dotenv import load_dotenv
+import os
+import pymongo
+import datetime
+
+def calculate_reminder_time(hari_reservasi):
+    hari_reservasi_datetime = datetime.datetime.strptime(hari_reservasi, '%Y-%m-%d')
+    try:
+        timestamp = hari_reservasi_datetime.timestamp()
+    except Exception as e:
+        return e
+    
+    return int(timestamp)
+    
+
+def main(args):
+    id_pasien = args.get('idPasien')
+    text = args.get('isiPesan')
+    hari_reservasi = args.get('tglKirim')[:10]
+
+    if not id_pasien:
+        return {"body": {"message": "ifPasien needed"}, "statusCode": 401}
+
+    try:
+        id_pasien = int(id_pasien)
+    except ValueError:
+        return {"body": {"message": "invalid idPasien"}, "statusCode": 401}
+
+    if not text:
+        return {"body": {"message": "isiPesan needed"}, "statusCode": 401}
+    
+    if not hari_reservasi:
+        return {"body": {"message": "tglKirim needed"}, "statusCode": 401}
+    
+    try:
+        load_dotenv()
+        mongodb_uri = os.getenv("MONGODB_URI")
+    except Exception as e:
+        return {"body": {"message": f"error {e}"}, "statusCode": 401}
+
+    client = pymongo.MongoClient(mongodb_uri)
+    db = client.mydb
+    reminder_collection = db.reminder
+    pasien_collection = db.pasien
+
+    timestamp = calculate_reminder_time(hari_reservasi)
+    pasien = pasien_collection.find_one({"id_pasien": id_pasien})
+    if not pasien:
+        return {"body": {"message": "pasien not found"}, "statusCode": 401}
+
+    if type(timestamp) != int:
+        return {"body": {"message": f"error {timestamp}"}, "statusCode": 401}
+
+    nama = pasien["nama_pasien"]
+
+    if pasien["data_kb"]:
+        try:
+            phone_number = pasien["no_hp"]    
+        except:
+            return {"body": {"message": "phone_number data not found"}, "statusCode": 401}
+    elif pasien["data_imunisasi"]:
+        try:
+            phone_number = pasien["no_hp"]    
+        except:
+            return {"body": {"message": "phone_number data not found"}, "statusCode": 401}
+    elif pasien["data_kehamilan"]:
+        try:
+            phone_number = pasien["data_kehamilan"]["section2"]["noTelp"]
+        except:
+            return {"body": {"message": "phone_number data not found"}, "statusCode": 401}
+    
+    json_data = {
+        "nama": nama,
+        "noHP": phone_number,
+        "text": text,
+        "remind_timestamp": timestamp,
+        "status": "reminder layanan"
+    }
+    session = client.start_session()
+    session.start_transaction()
+
+    try:
+        reminder_collection.insert_one(json_data, session=session)
+        
+        session.commit_transaction()
+        return {"body": {"message": "success"}, "statusCode": 200}
+    except Exception as e:
+        session.abort_transaction()
+        return {"body": {"message": f"create reminder failed: {e}"}, "statusCode": 401}
+    finally:
+        session.end_session()
